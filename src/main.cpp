4734fbf2928fdf5d699a98f06fa28680d1bf6839
Keyn Kim <78714522+keynkim@users.noreply.github.com>
2025-02-01 14:10:11 -0800

    start testing grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include "bn_core.h"
+#include "bn_bg_palettes.h"
+#include "bn_regular_bg_ptr.h"
+#include "bn_sprite_ptr.h"
+#include "bn_sprite_items_cell.h"
 
 int main()
 {
     bn::core::init();
-
+    bn::bg_palettes::set_transparent_color(bn::color(5, 5, 5));
+    bn::sprite_ptr sprite = bn::sprite_items::cell.create_sprite(-116, -76);
     while(true)
     {
         bn::core::update();
